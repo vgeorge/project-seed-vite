b392c617c7cc1b90907701d3cc14c72ddba37f6c
Vitor George <vitor.george@gmail.com>
2023-02-08 17:37:55 -0300

    Add ui-seed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+import { DevseedUiThemeProvider } from '@devseed-ui/theme-provider'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DevseedUiThemeProvider>
+      <App />
+    </DevseedUiThemeProvider>
   </React.StrictMode>,
 )
